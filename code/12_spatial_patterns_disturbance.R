38df09c70d93844bd0f2ce3edec872685b788b98
matthewbitters <matthew.bitters@colorado.edu>
2026-02-27 21:51:51 +0000

    spatial fig

--- code/12_spatial_patterns_disturbance.R
+++ code/12_spatial_patterns_disturbance.R
@@ -92,7 +92,7 @@ raster_paths <- list(
 forest <- rast(forest_mask)
 
 # Aggregation factor for faster plotting
-agg_factor <- 5
+agg_factor <- 10
 
 # Aggregate forest mask
 forest_ds <- aggregate(forest, fact = agg_factor, fun = max)  # small enough for plotting
@@ -119,6 +119,11 @@ west_eco <- west_eco %>%
 # Bounding box for plotting
 west_extent <- st_bbox(west_eco)
 
+# Outline of western us for plotting
+west_outline <- west_eco |>
+  st_union() |>
+  st_as_sf()
+
 # ===========================================================
 # Function to prepare raster for plotting
 # ===========================================================
@@ -139,7 +144,7 @@ raster_dfs <- lapply(raster_paths, raster_to_df)
 # ===========================================================
 base_map <- list(
   geom_raster(data = forest_df, aes(x = x, y = y),
-              fill = "grey88", alpha = 1),
+              fill = "grey95", alpha = 1),
   coord_sf(
     xlim = c(west_extent["xmin"], west_extent["xmax"]),
     ylim = c(west_extent["ymin"], west_extent["ymax"])
@@ -150,7 +155,7 @@ base_map <- list(
     plot.background  = element_rect(fill = "white", color = NA),
     legend.position = "bottom",
     legend.title = element_blank(),
-    legend.text = element_text(size = 7),
+    legend.text = element_text(size = 6),
     legend.key.size = unit(0.4, "cm")
   )
 )
@@ -161,20 +166,28 @@ colors_df <- bind_rows(
   raster_dfs$wf_any %>% mutate(disturbance = "WF (any)"),
   raster_dfs$bt_any %>% mutate(disturbance = "BT (any)"),
   raster_dfs$hd_any %>% mutate(disturbance = "HD (any)"),
+  raster_dfs$pd_any %>% mutate(disturbance = "PD (any)"),
   
   raster_dfs$wf_extr %>% mutate(disturbance = "WF (extr)"),
   raster_dfs$bt_extr %>% mutate(disturbance = "BT (extr)"),
   raster_dfs$hd_extr %>% mutate(disturbance = "HD (extr)"),
+  raster_dfs$pd_extr %>% mutate(disturbance = "PD (extr)"),
 
   raster_dfs$wf_hd_any %>% mutate(disturbance = "WF + HD (any)"),
   raster_dfs$bt_hd_any %>% mutate(disturbance = "BT + HD (any)"),
   raster_dfs$wf_bt_any %>% mutate(disturbance = "WF + BT (any)"),
+  raster_dfs$wf_pd_any %>% mutate(disturbance = "WF + PD (any)"),
+  raster_dfs$bt_pd_any %>% mutate(disturbance = "BT + PD (any)"),
   raster_dfs$wf_bt_hd_any %>% mutate(disturbance = "WF + BT + HD (any)"),
+  raster_dfs$wf_bt_pd_any %>% mutate(disturbance = "WF + BT + PD (any)"),
   
   raster_dfs$wf_hd_extr %>% mutate(disturbance = "WF + HD (extr)"),
   raster_dfs$bt_hd_extr %>% mutate(disturbance = "BT + HD (extr)"),
   raster_dfs$wf_bt_extr %>% mutate(disturbance = "WF + BT (extr)"),
-  raster_dfs$wf_bt_hd_extr %>% mutate(disturbance = "WF + BT + HD (extr)")
+  raster_dfs$wf_pd_extr %>% mutate(disturbance = "WF + PD (extr)"),
+  raster_dfs$bt_pd_extr %>% mutate(disturbance = "BT + PD (extr)"),
+  raster_dfs$wf_bt_hd_extr %>% mutate(disturbance = "WF + BT + HD (extr)"),
+  raster_dfs$wf_bt_pd_extr %>% mutate(disturbance = "WF + BT + PD (extr)")
 )
 
 
@@ -182,6 +195,8 @@ colors_df <- bind_rows(
 # ===========================================================
 # Four-panel plot
 # ===========================================================
+
+# wf any/extr
 p_wf <- ggplot() +
   base_map +
   geom_raster(data = filter(colors_df, disturbance %in% c("WF (any)", "WF (extr)")), aes(x = x, y = y, fill = disturbance), alpha = 0.7) +
@@ -192,9 +207,10 @@ p_wf <- ggplot() +
       "WF (extr)"   = "orangered4"
     )
   ) +
+  geom_sf(data = west_eco, fill = NA, color = "grey75", linewidth = 0.08) +
+  geom_sf(data = west_outline, fill = NA, color = "black", linewidth = 0.2) +
   guides(fill = guide_legend(ncol = 1)) +
   theme(legend.position = "bottom") +
-  geom_sf(data = west_eco, fill = NA, color = "black", size = 0.1) +
   # Add scale bar and compass rose to just wf panel
   annotation_scale(
     location = "bl",
@@ -213,20 +229,23 @@ p_wf <- ggplot() +
     pad_y  = unit(0.5, "cm")   # ← this is the key
   )
 
+# bt any/extr
 p_bt <- ggplot() +
   base_map +
   geom_raster(data = filter(colors_df, disturbance %in% c("BT (any)", "BT (extr)")), aes(x = x, y = y, fill = disturbance), alpha = 0.7) +
   scale_fill_manual(
     name = NULL,
     values = c(
-      "BT (any)"    = "khaki1",
-      "BT (extr)"   = "khaki4"
+      "BT (any)"    = "goldenrod1",
+      "BT (extr)"   = "goldenrod4"
     )
   ) +
+  geom_sf(data = west_eco, fill = NA, color = "grey75", linewidth = 0.08) +
+  geom_sf(data = west_outline, fill = NA, color = "black", linewidth = 0.2) +
   guides(fill = guide_legend(ncol = 1)) +
-  theme(legend.position = "bottom") +
-  geom_sf(data = west_eco, fill = NA, color = "black", size = 0.1)
+  theme(legend.position = "bottom")
 
+# hd any/extr
 p_hd <- ggplot() +
   base_map +
   geom_raster(data = filter(colors_df, disturbance %in% c("HD (any)", "HD (extr)")), aes(x = x, y = y, fill = disturbance), alpha = 0.7) +
@@ -237,10 +256,12 @@ p_hd <- ggplot() +
       "HD (extr)"   = "dodgerblue4"
     )
   ) +
+  geom_sf(data = west_eco, fill = NA, color = "grey75", linewidth = 0.08) +
+  geom_sf(data = west_outline, fill = NA, color = "black", linewidth = 0.2) +
   guides(fill = guide_legend(ncol = 1)) +
-  theme(legend.position = "bottom") +
-  geom_sf(data = west_eco, fill = NA, color = "black", size = 0.1)
+  theme(legend.position = "bottom")
 
+# wf, bt, hd any/extr
 p_wf_bt_hd <- ggplot() +
   base_map +
   geom_raster(data = filter(colors_df, disturbance %in% c("WF + BT + HD (any)", "WF + BT + HD (extr)")), aes(x = x, y = y, fill = disturbance), alpha = 0.7) +
@@ -251,9 +272,10 @@ p_wf_bt_hd <- ggplot() +
       "WF + BT + HD (extr)"   = "darkorchid4"
     )
   ) +
+  geom_sf(data = west_eco, fill = NA, color = "grey75", linewidth = 0.08) +
+  geom_sf(data = west_outline, fill = NA, color = "black", linewidth = 0.2) +
   guides(fill = guide_legend(ncol = 1)) +
-  theme(legend.position = "bottom") +
-  geom_sf(data = west_eco, fill = NA, color = "black", size = 0.1)
+  theme(legend.position = "bottom")
 
 
 # Combine panels
@@ -280,63 +302,159 @@ ggsave(
 # ===========================================================
 # Eight-panel plot
 # ===========================================================
+
+### Any disturbance
+
+# wf any
 p_wf_any <- ggplot() +
   base_map +
-  geom_raster(data = raster_dfs$wf_any, aes(x = x, y = y), fill = "orangered1", alpha = 0.7) +
-  geom_sf(data = west_eco, fill = NA, color = "black", size = 0.1) +
-  labs(title = "wf")
+  geom_raster(data = filter(colors_df, disturbance %in% c("WF (any)")), aes(x = x, y = y, fill = disturbance), alpha = 0.7) +
+  scale_fill_manual(
+    name = NULL,
+    values = c(
+      "WF (any)"    = "orangered1"
+    )
+  ) +
+  geom_sf(data = west_eco, fill = NA, color = "grey75", linewidth = 0.08) +
+  geom_sf(data = west_outline, fill = NA, color = "black", linewidth = 0.2) +
+  guides(fill = guide_legend(ncol = 1)) +
+  theme(legend.position = "bottom") +
+  # Add scale bar and compass rose to just wf panel
+  annotation_scale(
+    location = "bl",
+    width_hint = 0.2,
+    text_cex = 0.4,
+    pad_x = unit(0.2, "cm"),
+    pad_y = unit(0.2, "cm")
+  ) +
+  annotation_north_arrow(
+    location = "bl",
+    which_north = "true",
+    style = north_arrow_fancy_orienteering(text_size = 6),
+    height = unit(0.5, "cm"),
+    width  = unit(0.5, "cm"),
+    pad_x  = unit(0.2, "cm"),
+    pad_y  = unit(0.5, "cm")   # ← this is the key
+  )
 
+# bt any
 p_bt_any <- ggplot() +
   base_map +
-  geom_raster(data = raster_dfs$bt_any, aes(x = x, y = y), fill = "khaki1", alpha = 0.7) +
-  geom_sf(data = west_eco, fill = NA, color = "black", size = 0.1) +
-  labs(title = "bt")
+  geom_raster(data = filter(colors_df, disturbance %in% c("BT (any)")), aes(x = x, y = y, fill = disturbance), alpha = 0.7) +
+  scale_fill_manual(
+    name = NULL,
+    values = c(
+      "BT (any)"    = "goldenrod1"
+    )
+  ) +
+  geom_sf(data = west_eco, fill = NA, color = "grey75", linewidth = 0.08) +
+  geom_sf(data = west_outline, fill = NA, color = "black", linewidth = 0.2) +
+  guides(fill = guide_legend(ncol = 1)) +
+  theme(legend.position = "bottom")
 
+# hd any
 p_hd_any <- ggplot() +
   base_map +
-  geom_raster(data = raster_dfs$hd_any, aes(x = x, y = y), fill = "dodgerblue1", alpha = 0.7) +
-  geom_sf(data = west_eco, fill = NA, color = "black", size = 0.1) +
-  labs(title = "hd")
+  geom_raster(data = filter(colors_df, disturbance %in% c("HD (any)")), aes(x = x, y = y, fill = disturbance), alpha = 0.7) +
+  scale_fill_manual(
+    name = NULL,
+    values = c(
+      "HD (any)"    = "dodgerblue1"
+    )
+  ) +
+  geom_sf(data = west_eco, fill = NA, color = "grey75", linewidth = 0.08) +
+  geom_sf(data = west_outline, fill = NA, color = "black", linewidth = 0.2) +
+  guides(fill = guide_legend(ncol = 1)) +
+  theme(legend.position = "bottom")
 
-p_combo_any <- ggplot() +
+# combos any
+p_wf_bt_hd_any <- ggplot() +
   base_map +
-  geom_raster(data = raster_dfs$wf_hd_any, aes(x = x, y = y), fill = "firebrick1", alpha = 0.7) +
-  geom_raster(data = raster_dfs$bt_hd_any, aes(x = x, y = y), fill = "darkolivegreen1", alpha = 0.7) +
-  geom_raster(data = raster_dfs$wf_bt_any, aes(x = x, y = y), fill = "darkorchid1", alpha = 0.7) +
-  geom_raster(data = raster_dfs$wf_bt_hd_any, aes(x = x, y = y), fill = "darkgoldenrod1", alpha = 0.7) +
-  geom_sf(data = west_eco, fill = NA, color = "black", size = 0.1) +
-  labs(title = "int")
+  geom_raster(data = filter(colors_df, disturbance %in% c("WF + BT (any)", "WF + HD (any)", "BT + HD (any)", "WF + BT + HD (any)")), aes(x = x, y = y, fill = disturbance), alpha = 0.7) +
+  scale_fill_manual(
+    name = NULL,
+    values = c(
+      "WF + BT (any)"      = "firebrick1",
+      "WF + HD (any)"      = "darkolivegreen1",
+      "BT + HD (any)"      = "darkorchid1",
+      "WF + BT + HD (any)" = "darkgoldenrod1"
+    )
+  ) +
+  geom_sf(data = west_eco, fill = NA, color = "grey75", linewidth = 0.08) +
+  geom_sf(data = west_outline, fill = NA, color = "black", linewidth = 0.2) +
+  guides(fill = guide_legend(ncol = 1)) +
+  theme(legend.position = "bottom")
 
 
+### Extreme disturbance
+
+# wf extr
 p_wf_extr <- ggplot() +
   base_map +
-  geom_raster(data = raster_dfs$wf_extr, aes(x = x, y = y), fill = "orangered4", alpha = 0.7) +
-  geom_sf(data = west_eco, fill = NA, color = "black", size = 0.1)
-  
+  geom_raster(data = filter(colors_df, disturbance %in% c("WF (extr)")), aes(x = x, y = y, fill = disturbance), alpha = 0.7) +
+  scale_fill_manual(
+    name = NULL,
+    values = c(
+      "WF (extr)"    = "orangered4"
+    )
+  ) +
+  geom_sf(data = west_eco, fill = NA, color = "grey75", linewidth = 0.08) +
+  geom_sf(data = west_outline, fill = NA, color = "black", linewidth = 0.2) +
+  guides(fill = guide_legend(ncol = 1)) +
+  theme(legend.position = "bottom")
+
+# bt extr
 p_bt_extr <- ggplot() +
   base_map +
-  geom_raster(data = raster_dfs$bt_extr, aes(x = x, y = y), fill = "khaki4", alpha = 0.7) +
-  geom_sf(data = west_eco, fill = NA, color = "black", size = 0.1)
-  
+  geom_raster(data = filter(colors_df, disturbance %in% c("BT (extr)")), aes(x = x, y = y, fill = disturbance), alpha = 0.7) +
+  scale_fill_manual(
+    name = NULL,
+    values = c(
+      "BT (extr)"    = "goldenrod4"
+    )
+  ) +
+  geom_sf(data = west_eco, fill = NA, color = "grey75", linewidth = 0.08) +
+  geom_sf(data = west_outline, fill = NA, color = "black", linewidth = 0.2) +
+  guides(fill = guide_legend(ncol = 1)) +
+  theme(legend.position = "bottom")
+
+# hd extr
 p_hd_extr <- ggplot() +
   base_map +
-  geom_raster(data = raster_dfs$hd_extr, aes(x = x, y = y), fill = "dodgerblue4", alpha = 0.7) +
-  geom_sf(data = west_eco, fill = NA, color = "black", size = 0.1)
-  
-p_combo_extr <- ggplot() +
-  base_map +
-  geom_raster(data = raster_dfs$wf_hd_extr, aes(x = x, y = y), fill = "firebrick4", alpha = 0.7) +
-  geom_raster(data = raster_dfs$bt_hd_extr, aes(x = x, y = y), fill = "darkolivegreen4", alpha = 0.7) +
-  geom_raster(data = raster_dfs$wf_bt_extr, aes(x = x, y = y), fill = "darkorchid4", alpha = 0.7) +
-  geom_raster(data = raster_dfs$wf_bt_hd_extr, aes(x = x, y = y), fill = "darkgoldenrod4", alpha = 0.7) +
-  geom_sf(data = west_eco, fill = NA, color = "black", size = 0.1)
-  
+  geom_raster(data = filter(colors_df, disturbance %in% c("HD (extr)")), aes(x = x, y = y, fill = disturbance), alpha = 0.7) +
+  scale_fill_manual(
+    name = NULL,
+    values = c(
+      "HD (extr)"    = "dodgerblue4"
+    )
+  ) +
+  geom_sf(data = west_eco, fill = NA, color = "grey75", linewidth = 0.08) +
+  geom_sf(data = west_outline, fill = NA, color = "black", linewidth = 0.2) +
+  guides(fill = guide_legend(ncol = 1)) +
+  theme(legend.position = "bottom")
 
+# combos extr
+p_wf_bt_hd_extr <- ggplot() +
+  base_map +
+  geom_raster(data = filter(colors_df, disturbance %in% c("WF + BT (extr)", "WF + HD (extr)", "BT + HD (extr)", "WF + BT + HD (extr)")), aes(x = x, y = y, fill = disturbance), alpha = 0.7) +
+  scale_fill_manual(
+    name = NULL,
+    values = c(
+      "WF + BT (extr)"      = "firebrick4",
+      "WF + HD (extr)"      = "darkolivegreen4",
+      "BT + HD (extr)"      = "darkorchid4",
+      "WF + BT + HD (extr)" = "darkgoldenrod4"
+    )
+  ) +
+  geom_sf(data = west_eco, fill = NA, color = "grey75", linewidth = 0.08) +
+  geom_sf(data = west_outline, fill = NA, color = "black", linewidth = 0.2) +
+  guides(fill = guide_legend(ncol = 1)) +
+  theme(legend.position = "bottom")
 
 
 # Combine panels
-eight_panel <- (p_wf_any | p_bt_any | p_hd_any | p_combo_any) / 
-               (p_wf_extr | p_bt_extr | p_hd_extr | p_combo_extr) +
+eight_panel <- (p_wf_any | p_bt_any | p_hd_any | p_wf_bt_hd_any) / 
+               (p_wf_extr | p_bt_extr | p_hd_extr | p_wf_bt_hd_extr) +
   theme(plot.margin = margin(.1, 1, .1, 1))
   
   
@@ -362,68 +480,6 @@ ggsave(
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-forest_mask <- rast("data/derived/resampled/forest_mask_30m_resampled.tif")
-
-wf_any = rast("data/derived/annual_stacks/binary/spatial_presence/any/wf_any_presence.tif")
-bt_any = rast("data/derived/annual_stacks/binary/spatial_presence/any/bt_any_presence.tif")
-hd_any = rast("data/derived/annual_stacks/binary/spatial_presence/any/hd_any_presence.tif")
-
-wf_extr = rast("data/derived/annual_stacks/binary/spatial_presence/extreme/wf_extreme_presence.tif")
-bt_extr = rast("data/derived/annual_stacks/binary/spatial_presence/extreme/bt_extreme_presence.tif")
-hd_extr = rast("data/derived/annual_stacks/binary/spatial_presence/extreme/hd_extreme_presence.tif")
-
-
-# Quick base plot for each raster
-plot(wf_any, main="Wildfire Presence (Any)")
-plot(bt_any, main="Biotic Disturbance Presence (Any)")
-plot(hd_any, main="Hotter Drought Presence (Any)")
-
-
-
-
-
-# Plot
-# Forest mask
-plot(forest_mask, col=gray.colors(10, start=0.9, end=0.3), legend=FALSE)
-# Any
-plot(wf_any, col=adjustcolor("orange", alpha.f=0.5), add=TRUE, legend=FALSE)
-# Extreme
-plot(wf_extr, col=adjustcolor("orangered2", alpha.f=0.7), add=TRUE, legend=FALSE)
-
-
-
-plot(bt_any, col=adjustcolor("green", alpha.f=0.5), add=TRUE)
-
-plot(hd_any, col=adjustcolor("orange", alpha.f=0.5), add=TRUE)
-
-
-### Plot with ggplot
-
-# Downsample by factor of 100 (adjust as needed)
-forest_ds <- aggregate(forest_mask, fact=100, fun=mean)
-wf_ds <- aggregate(wf_pa, fact=100, fun=max)
-bt_ds <- aggregate(bt_pa, fact=100, fun=max)
-hd_ds <- aggregate(hd_pa, fact=100, fun=max)
-pd_ds <- aggregate(pd_pa, fact=100, fun=max)
 
 
 
